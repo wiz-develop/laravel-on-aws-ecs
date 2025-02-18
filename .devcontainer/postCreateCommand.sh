3ff4131b7161dc05175930fe5d952318414e74a2
垣内 繁直 <kakiuchi-shigenao@wiznet.co.jp>
2025-02-18 15:38:47 +0000

    PHPStan/PHPUnitを追加 Fixes #3

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
+# プロジェクトのソースコードディレクトリに移動
 cd ${CONTAINER_SRC_PATH}
+
+# Automatic PHPDoc generation for Laravel Facades
+# https://github.com/barryvdh/laravel-ide-helper?tab=readme-ov-file#automatic-phpdoc-generation-for-laravel-facades
+php artisan ide-helper:generate
--- src/tests/Unit/ExampleTest.php
+++ src/tests/Unit/ExampleTest.php
@@ -1,16 +1,20 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests\Unit;
 
+use DateTimeImmutable;
 use PHPUnit\Framework\TestCase;
 
-class ExampleTest extends TestCase
+final class ExampleTest extends TestCase
 {
     /**
      * A basic test example.
      */
     public function test_that_true_is_true(): void
     {
-        $this->assertTrue(true);
+        $date = new DateTimeImmutable();
+        $this->assertTrue($date->format('Y') === (string)date('Y'));
     }
 }
